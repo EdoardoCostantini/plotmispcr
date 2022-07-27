49cbc0e7e1fcb27ff6ecadec8d5f37a6c91ae7a7
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-07-27 17:30:06 +0200

    initial commit

---
+++ server.R
@@ -0,0 +1,83 @@
+# Project:   mi-spcr
+# Objective: TODO
+# Author:    Edoardo Costantini
+# Created:   2022-07-27
+# Modified:  2022-07-27
+
+library(shiny)
+library(ggplot2)
+library(shinyWidgets)
+library(dplyr)
+
+gg_shape <- readRDS("./20220726-143214-small-run-for-plot-development-pc-main-res.rds")
+
+plot_x_axis <- "npcs"
+plot_y_axis <- "coverage"
+moderator <- "method"
+grid_x_axis <- "mech"
+grid_y_axis <- "pm"
+
+server <- function(input, output, session) {
+
+  # Dynamically update inputs
+  observe({
+    # Statistics and Variables requested
+    if(any(input$stat %in% c("cor", "cov"))){
+      updateSelectInput(session,
+                        "vars",
+                        choices = unique(gg_shape$vars)[1:3]
+      )
+    } else {
+      updateSelectInput(session,
+                        "vars",
+                        choices = unique(gg_shape$vars)[4:6]
+      )
+    }
+
+    # Number of components displayed by slider based on nla condition
+    npcs_to_plot <- unique((gg_shape %>% filter(nla == input$nla))$npcs)
+    npcs_to_plot <- sort(npcs_to_plot)
+    shinyWidgets::updateSliderTextInput(session,
+                                        inputId = "npcs",
+                                        choices = npcs_to_plot,
+                                        selected = max(npcs_to_plot))
+  })
+
+  output$plot <- renderPlot(
+    res = 96,
+  {
+    gg_shape %>%
+      filter(nla == input$nla,
+             mech %in% input$mech,
+             pm %in% input$pm,
+             vars == input$vars,
+             stat == input$stat,
+             method %in% input$method,
+             npcs <= input$npcs) %>%
+      ggplot(aes_string(x = plot_x_axis,
+                        y = input$plot_y_axis,
+                        group = moderator)) +
+      geom_point(aes_string(shape = moderator), size = 1.5) +
+      geom_line(aes_string(lines = moderator)) +
+      scale_x_continuous(breaks = sort(unique(gg_shape$npcs)), sort(unique(gg_shape$npcs))) +
+      facet_grid(reformulate(grid_x_axis,
+                             grid_y_axis),
+                 labeller = labeller(.rows = label_both,
+                                     .cols = label_value),
+                 switch = "y") +
+      theme(
+        # Text
+        text = element_text(size = 12),
+        strip.text.y.right = element_text(angle = 0),
+        plot.title = element_text(hjust = 0.5),
+        axis.title = element_text(size = 10),
+        # Legend
+        legend.title = element_blank(),
+        legend.position = "bottom",
+        # Backgorund
+        panel.background = element_rect(fill = NA, color = "gray")
+      )
+  }
+  )
+
+}
---
+++ ui.R
@@ -0,0 +1,59 @@
+# Project:   shiny-mi-spcr-plot
+# Objective: TODO
+# Author:    Edoardo Costantini
+# Created:   2022-07-27
+# Modified:  2022-07-27
+
+library(shiny)
+library(ggplot2)
+library(shinyWidgets)
+library(dplyr)
+
+gg_shape <- readRDS("./20220726-143214-small-run-for-plot-development-pc-main-res.rds")
+
+plot_x_axis <- "npcs"
+plot_y_axis <- "coverage"
+moderator <- "method"
+grid_x_axis <- "mech"
+grid_y_axis <- "pm"
+
+ui <- fluidPage(
+  titlePanel("Plotting results for study"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("nla",
+                  "Number of latent variables",
+                  choices = sort(unique(gg_shape$nla))),
+      checkboxGroupInput("pm",
+                         "Proportion of missing values",
+                         choices = sort(unique(gg_shape$pm)),
+                         selected = sort(unique(gg_shape$pm))),
+      checkboxGroupInput("mech",
+                         "Missing data mechanism",
+                         inline = TRUE,
+                         choices = levels(gg_shape$mech),
+                         selected = levels(gg_shape$mech)),
+      selectInput("stat",
+                  "Statistic",
+                  choices = unique(gg_shape$stat)),
+      selectInput("vars",
+                  "Variables",
+                  choices = unique(gg_shape$vars)),
+      selectInput("plot_y_axis",
+                  "Outcome measure",
+                  choices = c("RB", "PRB", "coverage", "CIW_avg", "mcsd")),
+      checkboxGroupInput("method", "Imputation methods to compare:",
+                         choices = levels(gg_shape$method),
+                         selected = levels(gg_shape$method)),
+      shinyWidgets::sliderTextInput(inputId = "npcs",
+                                    label = "Number of principal components",
+                                    hide_min_max = TRUE,
+                                    choices = sort(unique(gg_shape$npcs)),
+                                    selected = max(unique(gg_shape$npcs)),
+                                    grid = TRUE),
+    ),
+    mainPanel(
+      plotOutput("plot", height = "800px")
+    )
+  )
+)
