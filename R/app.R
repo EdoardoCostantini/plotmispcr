a489fd710ddeaab51c5217106a3c127d72eb5d4e
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-04-28 13:01:10 +0200

    feat: modulirise ui

--- R/app.R
+++ R/app.R
@@ -65,182 +65,6 @@ plotResults <- function() {
     grid_x_axis <- "mech"
     grid_y_axis <- "pm"
 
-    # UI -----------------------------------------------------------------------
-
-    # App title
-
-    ui <- fluidPage(
-        fluidRow(
-            shiny::titlePanel(
-                shiny::h1("MI-SPCR simulation study results", align = "center")
-            ),
-            shiny::column(
-                width = 10,
-                offset = 1,
-                # Create tabs for different plotting aspects
-                shiny::tabsetPanel(
-                    type = "tabs",
-                    selected = "About this Shiny app",
-                    shiny::tabPanel(
-                        title = "About this Shiny app",
-                        "Coming soon"
-                    ),
-                    shiny::tabPanel(
-                        title = "Simulation study",
-                        shiny::fluidRow(
-                            shiny::column(
-                                width = 4,
-                                shiny::titlePanel(
-                                    shiny::h3("Simulation  study", align = "center")
-                                ),
-                                shiny::tabsetPanel(
-                                    type = "tabs",
-                                    shiny::tabPanel(
-                                        title = "Introduction",
-                                        shiny::htmlOutput("introduction")
-                                    ),
-                                    shiny::tabPanel(
-                                        title = "Outcome measures",
-                                        selectInput(
-                                            inputId = "plot_sim_y_axis",
-                                            label = "Outcome measure",
-                                            choices = c("RB", "PRB", "CIC", "CIW", "mcsd")
-                                        ),
-                                        radioButtons(
-                                            inputId = "plot_sim_stat",
-                                            label = "Statistic",
-                                            inline = TRUE,
-                                            choices = unique(dataResults$stat)
-                                        ),
-                                        radioButtons(
-                                            inputId = "plot_sim_vars",
-                                            label = "Variables",
-                                            inline = TRUE,
-                                            choices = unique(dataResults$vars)
-                                        ),
-                                        shinyWidgets::sliderTextInput(
-                                            inputId = "plot_sim_y_range",
-                                            label = "Y-axis range",
-                                            hide_min_max = FALSE,
-                                            choices = 0:100,
-                                            selected = c(0, 10),
-                                            grid = FALSE
-                                        )
-                                    ),
-                                    shiny::tabPanel(
-                                        title = "Data generation",
-                                        radioButtons(
-                                            inputId = "plot_sim_nla",
-                                            label = "Number of latent variables",
-                                            choices = sort(unique(dataResults$nla)),
-                                            inline = TRUE
-                                        ),
-                                        checkboxGroupInput(
-                                            inputId = "plot_sim_pm",
-                                            label = "Proportion of missing values",
-                                            choices = sort(unique(dataResults$pm)),
-                                            selected = sort(unique(dataResults$pm)),
-                                            inline = TRUE
-                                        ),
-                                        checkboxGroupInput("plot_sim_mech",
-                                            "Missing data mechanism",
-                                            inline = TRUE,
-                                            choices = levels(dataResults$mech),
-                                            selected = levels(dataResults$mech)
-                                        )
-                                    ),
-                                    shiny::tabPanel(
-                                        title = "Missing data treatments",
-                                        checkboxGroupInput("plot_sim_method",
-                                            "Imputation methods to compare:",
-                                            choices = levels(dataResults$method),
-                                            selected = levels(dataResults$method)[1:4],
-                                            inline = TRUE
-                                        ),
-                                        shinyWidgets::sliderTextInput(
-                                            inputId = "plot_sim_npcs",
-                                            label = "Number of principal components",
-                                            hide_min_max = TRUE,
-                                            choices = sort(unique(dataResults$npcs)),
-                                            selected = range(dataResults$npcs),
-                                            grid = TRUE
-                                        )
-                                    )
-                                )
-                            ),
-                            shiny::column(
-                                width = 8,
-                                shiny::fluidRow(
-                                    shiny::titlePanel(
-                                        shiny::h3("Plots", align = "center")
-                                    ),
-                                    shiny::plotOutput("plot"),
-
-                                    # Silent extraction of size
-                                    shinybrowser::detect(),
-                                ),
-                                style = "border-left: 1px solid; border-left-color: #DDDDDD"
-                            )
-                        )
-                    ),
-                    shiny::tabPanel(
-                        title = "Convergence checks",
-                        shiny::fluidRow(
-                            shiny::column(
-                                width = 4,
-                                shiny::titlePanel(
-                                    shiny::h3("Simulation  study", align = "center")
-                                ),
-                                shiny::tabsetPanel(
-                                    type = "tabs",
-                                    shiny::tabPanel(
-                                        title = "Introduction",
-                                        ""
-                                    ),
-                                    shiny::tabPanel(
-                                        title = "Inputs",
-                                        selectInput("plot_case_method",
-                                            "Imputation method:",
-                                            choices = levels(dataResults$method)[1:7],
-                                            selected = levels(dataResults$method)[1]
-                                        ),
-                                        selectInput(
-                                            inputId = "plot_case_npcs",
-                                            label = "Number of PCs used",
-                                            choices = sort(unique(dataResults$npcs))[-1],
-                                            selected = sort(unique(dataResults$npcs))[2]
-                                        ),
-                                        shinyWidgets::sliderTextInput(
-                                            inputId = "plot_case_iters",
-                                            label = "Iteration range",
-                                            hide_min_max = TRUE,
-                                            choices = 0:100,
-                                            selected = c(0, 25),
-                                            grid = FALSE
-                                        )
-                                    )
-                                )
-                            ),
-                            shiny::column(
-                                width = 8,
-                                shiny::fluidRow(
-                                    shiny::titlePanel(
-                                        shiny::h3("Plots", align = "center")
-                                    ),
-                                    shiny::plotOutput("plot_mids"),
-
-                                    # Silent extraction of size
-                                    shinybrowser::detect(),
-                                ),
-                                style = "border-left: 1px solid; border-left-color: #DDDDDD"
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-
     # Server -------------------------------------------------------------------
 
     server <- function(input, output, session) {
@@ -345,15 +169,15 @@ plotResults <- function() {
                     geom_line(col = "darkgray") +
                     geom_point(aes_string(shape = moderator), size = 2.5) +
                     scale_x_continuous(
-                        breaks = sort(unique(dataResults$npcs)), 
+                        breaks = sort(unique(dataResults$npcs)),
                         sort(unique(dataResults$npcs))
-                        ) +
+                    ) +
 
                     # Zoomable y-axis
                     coord_cartesian(ylim = c(
-                        input$plot_sim_y_range[1], 
-                        input$plot_sim_y_range[2])
-                        ) +
+                        input$plot_sim_y_range[1],
+                        input$plot_sim_y_range[2]
+                    )) +
 
                     # Facet grid
                     facet_grid(
@@ -383,7 +207,7 @@ plotResults <- function() {
             }
         )
 
-output$plot_mids <- renderPlot(
+        output$plot_mids <- renderPlot(
             res = 96,
             height = 750,
             {
@@ -466,11 +290,13 @@ output$plot_mids <- renderPlot(
                     par.strip.text = list(lines = 0.5),
                 )
             }
-
         )
     }
 
     # Run app ------------------------------------------------------------------
 
-    shinyApp(ui, server)
+    shinyApp(
+        ui = ui_call(),
+        server
+    )
 }
